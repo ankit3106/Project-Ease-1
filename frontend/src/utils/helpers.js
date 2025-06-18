6d564da5f6d18467e6f0518abe7c9dcb3b6d15cd
Ankit <120703430+ankit3106@users.noreply.github.com>
2025-06-18 14:33:03 +0530

    Update helpers.js

--- frontend/src/utils/helpers.js
+++ frontend/src/utils/helpers.js
@@ -1,29 +1,29 @@
-// import moment from "moment";
-
-// export const getAntdFormInputRules = [
-//   {
-//     required: true,
-//     message: "Required",
-//   },
-// ];
-
-// export const getDateFormat = (date) => {
-//   return moment(date).format("MMMM Do YYYY, h:mm A");
-// }
-
-import dayjs from "dayjs";
-import advancedFormat from "dayjs/plugin/advancedFormat";
-
-// Extend dayjs to support advanced formatting (e.g., "Do")
-dayjs.extend(advancedFormat);
+import moment from "moment";
 
 export const getAntdFormInputRules = [
-    {
-        required: true,
-        message: "Required",
-    },
+  {
+    required: true,
+    message: "Required",
+  },
 ];
 
 export const getDateFormat = (date) => {
-    return dayjs(date).format("MMMM Do YYYY, h:mm A");
+  return moment(date).format("MMMM Do YYYY, h:mm A");
 }
+
+// import dayjs from "dayjs";
+// import advancedFormat from "dayjs/plugin/advancedFormat";
+
+// // Extend dayjs to support advanced formatting (e.g., "Do")
+// dayjs.extend(advancedFormat);
+
+// export const getAntdFormInputRules = [
+//     {
+//         required: true,
+//         message: "Required",
+//     },
+// ];
+
+// export const getDateFormat = (date) => {
+//     return dayjs(date).format("MMMM Do YYYY, h:mm A");
+// }
