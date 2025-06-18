f23dd373b2a9b92b7aa6684813dd622973405d7d
Ankit <luffytarow13@gmail.com>
2025-06-18 14:09:39 +0530

    changes to server.js

--- backend/server.js
+++ backend/server.js
@@ -60,7 +60,7 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:4000", // adjust if your frontend uses a different port
+    origin: "https://project-ease-1.vercel.app", // adjust if your frontend uses a different port
     methods: ["GET", "POST"],
   },
 });
